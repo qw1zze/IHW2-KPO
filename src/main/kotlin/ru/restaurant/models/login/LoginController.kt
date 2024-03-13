d9660d20678f04b9c9be55a122a1cfd8d17d149d
qw1zz <dakalyakin_1@edu.hse.ru>
2024-03-13 04:31:23 +0300

    login from bd

---
+++ src/main/kotlin/ru/restaurant/models/login/LoginController.kt
@@ -0,0 +1,41 @@
+package ru.restaurant.models.login
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import ru.restaurant.cache.InMemoryCache
+import ru.restaurant.cache.TokenCache
+import ru.restaurant.database.tokens.TokenDTO
+import ru.restaurant.database.tokens.Tokens
+import ru.restaurant.database.users.Users
+import ru.restaurant.models.register.RegisterRecieveRemote
+import java.util.*
+
+class LoginController(private val call: ApplicationCall) {
+
+    suspend fun loginUser() {
+        val recieve = call.receive<LoginRecieveRemote>()
+        val userDTO = Users.getByLogin(recieve.login)
+
+        if (userDTO == null) {
+            call.respond(HttpStatusCode.BadRequest, message = "User not found")
+        } else {
+            if (userDTO.password == recieve.password) {
+                val token = UUID.randomUUID().toString()
+
+                Tokens.insert(
+                    TokenDTO(
+                        id = UUID.randomUUID().toString(),
+                        login = recieve.login,
+                        token = token)
+                )
+
+                call.respond(LoginResponseRemote(token = token))
+            } else {
+                call.respond(HttpStatusCode.BadRequest, "InvalidPassword")
+            }
+        }
+        call.respond(HttpStatusCode.BadRequest)
+    }
+}
--- src/main/kotlin/ru/restaurant/models/login/LoginRouting.kt
+++ src/main/kotlin/ru/restaurant/models/login/LoginRouting.kt
@@ -12,21 +12,8 @@ import java.util.UUID
 fun Application.configureLoginRouting() {
     routing {
         post("/login") {
-            val recieve = call.receive<LoginRecieveRemote>()
-            val first = InMemoryCache.userList.firstOrNull {it.login == recieve.login}
-
-            if (first == null) {
-                call.respond(HttpStatusCode.BadRequest, message = "User not found")
-            } else {
-                if (first.password == recieve.password) {
-                    val token = UUID.randomUUID().toString()
-                    InMemoryCache.token.add(TokenCache(login = recieve.login, token = token))
-                    call.respond(LoginResponseRemote(token = token))
-                } else {
-                    call.respond(HttpStatusCode.BadRequest, "InvalidPassword")
-                }
-            }
-            call.respond(HttpStatusCode.BadRequest)
+            val loginController = LoginController(call)
+            loginController.loginUser()
         }
     }
 }
